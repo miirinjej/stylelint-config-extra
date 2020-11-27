9a8720b3d039eb50ca067714cad2ff119935da84
miirinjej <miirinjej@gmail.com>
2020-11-27 18:50:33 +0500

    Feat: add config

---
+++ index.js
@@ -0,0 +1,51 @@
+module.exports = {
+  // add your custom config here
+  // https://stylelint.io/user-guide/configuration
+  extends: ['stylelint-8-point-grid'],
+  plugins: [
+    'stylelint-color-format',
+    'stylelint-declaration-block-no-ignored-properties',
+    'stylelint-declaration-strict-value',
+    'stylelint-high-performance-animation',
+  ],
+  rules: {
+    /**
+     * 8-point grid
+     */
+    'plugin/8-point-grid': {
+      base: 8,
+      allowlist: [],
+      ignorelist: [],
+    },
+
+    /**
+     * Color format
+     */
+    'color-format/format': {
+      format: 'hsl',
+    },
+
+    /**
+     * No ignored properties
+     */
+    'plugin/declaration-block-no-ignored-properties': true,
+
+    /**
+     * Strict value
+     */
+    'scale-unlimited/declaration-strict-value': [
+      ['/color$/', 'z-index', 'font-size'],
+      { disableFix: true },
+    ],
+
+    /**
+     * High performance animation
+     */
+    'plugin/no-low-performance-animation-properties': [
+      true,
+      {
+        ignoreProperties: [],
+      },
+    ],
+  },
+};
